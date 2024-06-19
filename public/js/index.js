c342f2ea0f12109200dea64ae5e1139838783966
Simon Malaret <simon@simonmt.com>
2024-06-19 11:46:33 +0200

    Working netlify env

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 const fetchButton = document.querySelector("#fetchButton");
 const responseText = document.querySelector("#responseText");
 fetchButton.addEventListener("click", async () => {
-  const response = await fetch("../netlify/functions/main.js").then(
+  const response = await fetch("/.netlify/functions/main").then(
     (response) => response.json()
   );
 
