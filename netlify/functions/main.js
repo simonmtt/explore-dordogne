03035b911f15f6a00ae719d082f61f944a6031c6
Simon Malaret <simon@simonmt.com>
2024-07-15 11:19:34 +0200

    working mvp after 3 hours. Items dynamically display.

--- netlify/functions/main.js
+++ netlify/functions/main.js
@@ -1,8 +1,84 @@
-export const handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello World!",
-    }),
-  };
+import { config } from "dotenv";
+import fetch from "node-fetch";
+
+config();
+
+const AIRTABLE_BASE_URL = "https://api.airtable.com/v0";
+const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
+const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
+const AIRTABLE_PROG_TABLE_ID = process.env.AIRTABLE_PROG_TABLE_ID;
+const AIRTABLE_RECO_TABLE_ID = process.env.AIRTABLE_RECO_TABLE_ID;
+
+export const handler = async (event) => {
+  try {
+    const { id } = JSON.parse(event.body);
+
+    const progUrl = `${AIRTABLE_BASE_URL}/${AIRTABLE_BASE_ID}/${AIRTABLE_PROG_TABLE_ID}/${id}`;
+    const progResponse = await fetch(progUrl, {
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+      },
+    });
+
+    if (!progResponse.ok) {
+      throw new Error(`Failed to fetch data: ${progResponse.statusText}`);
+    }
+
+    const progData = await progResponse.json();
+    // console.log(progData);
+    const recommandationIds = progData.fields["Recommandations"];
+
+    // Airtable does not return property if it is empty. Hence we check for existance of variable instead of !length>0
+    if (!recommandationIds) {
+      throw new Error(
+        `Failed to fetch data: Program has no linked recommandations.`
+      );
+    }
+
+    const recoOutput = await fetchRecommendations(recommandationIds);
+    if (!recoOutput.length > 0) {
+      throw new Error(`Failed to fetch data. Recommandations are empty.`);
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(recoOutput),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
 };
+
+async function fetchRecommendations(recommandationIds) {
+  const recoPromises = recommandationIds.map(async (recoId) => {
+    const recoUrl = `${AIRTABLE_BASE_URL}/${AIRTABLE_BASE_ID}/${AIRTABLE_RECO_TABLE_ID}/${recoId}`;
+
+    try {
+      const recoResponse = await fetch(recoUrl, {
+        headers: {
+          Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+        },
+      });
+
+      if (recoResponse.ok) {
+        return await recoResponse.json();
+      } else {
+        console.error(
+          `Failed to fetch data for recoId ${recoId}: ${recoResponse.statusText}`
+        );
+        return null;
+      }
+    } catch (error) {
+      console.error(
+        `Failed to fetch data for recoId ${recoId}: ${error.message}`
+      );
+      return null;
+    }
+  });
+
+  const recoOutput = (await Promise.all(recoPromises)).filter(Boolean); // Filter out null values
+  return recoOutput;
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,49 @@
-const fetchButton = document.querySelector("#fetchButton");
-const responseText = document.querySelector("#responseText");
-fetchButton.addEventListener("click", async () => {
-  const response = await fetch("/.netlify/functions/main").then(
-    (response) => response.json()
-  );
+const searchParams = new URLSearchParams(window.location.search);
+const progId = searchParams.get("id");
 
-  responseText.innerText = response;
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+
+    const response = await fetch("/.netlify/functions/main", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: progId }),
+    });
+
+    const recoData = await response.json();
+    console.log(recoData);
+
+    if (recoData.error) {
+      throw new Error(recoData.error);
+    }
+
+    const ulElement = document.querySelector("#responseList");
+    if (ulElement) {
+      ulElement.innerHTML = buildListItems(recoData);
+    }
+
+  } catch (error) {
+    //
+  }
 });
+
+function buildListItems(dataArray) {
+  return dataArray
+    .map(
+      (data) => `
+    <li class="list-group-item d-flex justify-content-between align-items-center" id="${data.id}">
+      ${
+        data.fields["Nom (from Adresse)"]
+          ? data.fields["Nom (from Adresse)"][0]
+          : "-"
+      }
+      <span class="badge text-bg-primary rounded-pill">${
+        data.fields.Index || 0
+      }</span>
+    </li>
+  `
+    )
+    .join("");
+}
